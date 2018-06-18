c8fbb699885c6aa1a1c0d43f117e34dcb949a726
Justin Patrin <papercrane@reversefold.com>
2018-06-18 09:45:48 -0700

    Bump to v0.5.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.4.3'
+VERSION = '0.5.0'
 
 setup(
     name='magicreq',
@@ -9,8 +9,8 @@ setup(
     author_email='papercrane@reversefold.com',
     maintainer='Justin Patrin',
     maintainer_email='papercrane@reversefold.com',
-    description='magicreq',
-    long_description="""magicreq""",
+    description='magicreq (Magic Requirements): Depend on packages without having to install them.',
+    long_description="""magicreq gives you a solution which takes only 15 (or so) lines added to the top of your script which will create a dynamic virtualenv with your specified requirements and automatically re-run the script in that virtualenv. It even works if you haven't installed magicreq.""",
     packages=find_packages(),
     url='https://github.com/reversefold/magicreq',
 )
